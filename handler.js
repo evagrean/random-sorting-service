de0506e783847c0f17ad816eb67ad3cbec21db46
eva <me@example.com>
2020-12-29 13:37:52 +0100

    added CORS headers to return statement

--- handler.js
+++ handler.js
@@ -18,6 +18,10 @@ module.exports.getRandomHouseAndHousemates = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
 
       body: JSON.stringify({
         randomHouse: randomHouse,
